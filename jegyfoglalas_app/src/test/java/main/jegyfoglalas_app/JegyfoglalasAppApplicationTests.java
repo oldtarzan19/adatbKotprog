f1d2bc7da780514a18ac97f5ca5118e24e63ac63
Zsofenszki Kristóf <h166567@stud.u-szeged.hu>
2022-11-16 19:03:39 +0100

    demo projekt feltöltése

---
+++ jegyfoglalas_app/src/test/java/main/jegyfoglalas_app/JegyfoglalasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package main.jegyfoglalas_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JegyfoglalasAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

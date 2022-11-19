8dae5d3637db72677d48a0e9047c69c89f64dfc8
Zsofenszki Kristóf <h166567@stud.u-szeged.hu>
2022-11-19 13:49:18 +0100

    Egy egyszerű Navigációs sáv és néhány css formázás

--- jegyfoglalas_app/src/main/java/application/controller/HtmlController.java
+++ jegyfoglalas_app/src/main/java/application/controller/HtmlController.java
@@ -5,8 +5,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HtmlController {
-    @GetMapping(value = "second")
-    public String get_second(){
-        return "second";
+
+    @GetMapping(value = "index_site")
+    public String getIndex(){
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "ugyfel_site")
+    public String getNewUgyfel(){
+        return "ugyfel";
+    }
+
+    @GetMapping(value = "jaratok_site")
+    public String getJaratok(){
+        return "jaratok";
+    }
+
+    @GetMapping(value = "foglalasok_site")
+    public String getFoglalasok(){
+        return "foglalasok";
+    }
+
+    @GetMapping(value = "szallasok_site")
+    public String getSzallasok(){
+        return "szallasok";
     }
 }
